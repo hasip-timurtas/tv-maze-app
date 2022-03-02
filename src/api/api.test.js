4a86a3b87281a077b8c3f1f69719d5cd10e7bd21
Hasip Timurtaş <hasip.timurtas@gmail.com>
2022-03-02 20:39:22 +0000

    Home mobile design and mixin implemented

--- src/api/api.test.js
+++ src/api/api.test.js
@@ -5,10 +5,16 @@ test('Api gets show list', async () => {
   expect(result.data.length).toBeGreaterThan(0);
 });
 
-
 test('Api gets show details', async () => {
   const showId = 6006;
   const showName = 'Dr. Phil';
   const result = await apiClient.getShowDetail(showId);
   expect(result.data.name).toEqual(showName)
+});
+
+test('Api gets cast details', async () => {
+  const showId = 6006;
+  const personName = 'Phil McGraw';
+  const result = await apiClient.getCasts(showId);
+  expect(result.data.person.name).toEqual(personName)
 });
