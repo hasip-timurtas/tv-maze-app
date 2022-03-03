19a47410d4882d49b0b7775038b6b310c3b5e2ba
Hasip Timurtaş <hasip.timurtas@gmail.com>
2022-03-03 10:34:06 +0000

    JSDoc added to removeTags

--- src/utils/removeTags.js
+++ src/utils/removeTags.js
@@ -1,3 +1,9 @@
+/**
+ * Removes html tags from the given text
+ * 
+ * @param {string} str 
+ * @returns {string} text without html elements
+ */
 export const removeTags = (str) => {
   if ((str === null) || (str === '')) {
     return false;
